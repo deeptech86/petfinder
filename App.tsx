e9cfd74f03e6e0e373c8213e674c97a5abf7769b
deeptech86 <deeptech870@gmail.com>
2022-07-02 01:05:57 -0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!1</h1>
+      <p>test</p>
+    </div>
+  );
+}
